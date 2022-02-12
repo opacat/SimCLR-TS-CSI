ccb30d66343fb76e66f3815621588d696e045d8b
Nicola Braile <nicola.braile@icloud.com>
2022-02-12 16:15:19 +0100

    final version l2r crop resize random noise

--- augm/augment.py
+++ augm/augment.py
@@ -12,41 +12,85 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+'''
+ The input matrix has T rows and M columns where:
+     T  = window size 
+     M  = number of features ( or channels )
+'''
 
-# Left to right flipping through antidiagonal matrix multiplication
+def apply_augm_xChannel(datas,augmentation):
+    augmented_datas = np.array(datas)
+    channels = datas.shape[1]
+    for chi in range(channels):
+       augmented_datas[chi] = augmentation(datas[:,chi])
+    return augmented_datas.transpose()
 
-def left2rightFlip(data):
-     dim = data.shape[0]    
-     antiD_Eye_Matrix = np.fliplr(np.eye(dim))
-     return data.dot(antiD_Eye_Matrix)
+# Left to right flipping through anti diagonal matrix multiplication
+def left2rightFlip(datas,show_plot=False):
+    
+    datas_ltor = np.array(datas) 
+    def _xChannel_left2rightFlip(channel):
+         dim = channel.shape[0]    
+         antiDiag_Eye_Matrix = np.fliplr(np.eye(dim))
+         return np.dot(channel,antiDiag_Eye_Matrix)
+    datas_ltor = apply_augm_xChannel(datas, _xChannel_left2rightFlip )
+    #plots first channel only for now
+    if show_plot:
+        plot_data(datas[:,0], datas_ltor[:,0]) 
+    
 
 # Interpolation and selection of a sub signal of size dim
-def crop_resize(data):
-    outdata=[]
-    dim = data.shape[0]    
-    #interpolation between odd values --> this generate a signal of 2*dim - 1 sample
-    for i in range(dim):
-        if i==0:
-            outdata.append(data[i])
-            continue
+def crop_resize(datas):
     
-        outdata.append( (data[i]+ data[i-1]) /2 )
-        outdata.append(data[i])
+    datas_cr = np.array(datas)
     
-    #selection of dim samples starting in a random point of the first window
-    start = random.randint(0, dim-1)
-    end = start+dim    
-    return outdata[start:end]
-
+    def _xChannel_crop_resize(channel):
+        outdata=[]
+        dim = channel.shape[0]    
+        #interpolation between odd values --> this generate a signal of 2*dim - 1 sample
+        for i in range(dim):
+            if i==0:
+                outdata.append(channel[i])
+                continue    
+            outdata.append( (channel[i]+ channel[i-1]) /2 )
+            outdata.append(channel[i])
+        
+        #selection of dim samples starting in a random point of the first window
+        start = random.randint(0, dim-1)
+        end = start+dim   
+        return outdata[start:end]
+    
+    datas_cr = apply_augm_xChannel(datas, _xChannel_crop_resize)
+   
+    plot_data(datas[:,0], datas_cr[:,0]) 
+    return datas_cr
 
 # Applies random noise to the signal by adding or removing std * p values 
 # where p is drawn from a uniform distribution
-def random_noise(data):
-    dim = data.shape[0]        
-    std = np.std(data)
-    prob = std*np.random.uniform(-1.0, 1.0 , dim)
-    return data*prob
-
+def random_noise(datas):
+    dim1 = datas.shape[0]        
+    dim2 = datas.shape[1]
+    datas_rn = np.array(datas)
+    std = np.std(datas,axis=1)
+    print("std")
+    print(std)
+    ru = np.random.uniform(-1.0, 1.0 , (dim1,dim2))
+    print("random uniform matrix ")
+    print(ru)
+    prob_matrix = std* ru
+    print("prob matrix ")
+    print(prob_matrix)
+    datas_rn = np.multiply(datas,prob_matrix)
+    print(datas_rn)
+    plot_data(datas[:,0], datas_rn[:,0]) 
+    
+    '''
+    def _xChannel_random_noise(channel):
+        dim = channel.shape[0]        
+        std = np.std(channel)
+        prob = std*np.random.uniform(-1.0, 1.0 , dim)
+        return channel*prob
+    '''
 
 #              HARD AUGMENTATIONS
 
@@ -74,13 +118,18 @@ def permute_channels(data):
     return x
     
 def plot_data(data,data_augm):
-    plt.plot(data)
-    plt.plot(data_augm)
+    fig = plt.figure()
+    x= [1,2,3]
+    ax = fig.add_subplot(111)
+    plt.plot(x,data)
+    plt.plot(x,data_augm)
+    ax.set_xlim([-10, 10])
+    ax.set_ylim([-10, 10])
     plt.show()  
 
 #TESTING CODE
 
-b = np.array([1,2,3,4,5,6,7,8,9])
+b = np.array([1.,2.,3.,4.,5.,6.,7.,8.,9.])
 #left2rightFlip(b)
 #crop_resize(b)
 #blockout(b, 5)
@@ -90,12 +139,12 @@ b = np.array([1,2,3,4,5,6,7,8,9])
 #plot_data(b,res)
 
 b = b.reshape((3, 3))
-print(b)
-r = permute_channels(b)
-print(r)
-'''
-arr = np.arange(9).reshape((3, 3))
-print(arr)
-np.random.shuffle(arr)
-print(arr)
-'''
+
+#r = permute_channels(b)
+#print(r)
+
+a = b
+#left2rightFlip(a,True)
+#crop_resize(a)
+
+random_noise(a)
