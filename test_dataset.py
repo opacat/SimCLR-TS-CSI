d58f3ad7cc1dbb808e88f3fca078a79cd27009cc
Marianna Del Corso <s255375@studenti.polito.it>
2022-02-13 16:54:07 +0100

    test dataset

--- test_dataset.py
+++ test_dataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 from augm.augment import *
 from augm.augmenter import *
+from tensorflow.keras.utils import timeseries_dataset_from_array
 
 data = np.load('dataset/nyc_taxi.npz')
 
@@ -8,6 +9,13 @@ training = data['training'][:50]
 training = np.expand_dims(training, axis=1)
 print(training.shape)
 
+#TEST WINDOWS
+train = data['training']
+print('training : ',train.shape)
+
+loader = timeseries_dataset_from_array(data=train,targets=None,sequence_length=10)
+for a,i in zip(loader,range(2)):
+  print(a)
 
 #TESTING CODE for Augment.py
 
@@ -24,14 +32,14 @@ magnitude_warping(training,2,3)
 #Notes:
 #CR must be checked because of zig zag visualitazion ( it may not be a fault )
 
-#augmenter(datas=training,is_hard_augm=False,hard_augm='',is_multiple_augm=True,soft_order=['CR','RN','L2R'],single_augm='') 
+#augmenter(datas=training,is_hard_augm=False,hard_augm='',is_multiple_augm=True,soft_order=['CR','RN','L2R'],single_augm='')
 #PASS
 
 #augmenter(datas=training,is_hard_augm=False,hard_augm='',is_multiple_augm=False,soft_order=[],single_augm='RN')
 #PASS
 
 #augmenter(datas=training,is_hard_augm=True,hard_augm='MW',is_multiple_augm=False,soft_order=[],single_augm='L2R')
-#PASS 
+#PASS
 
 #augmenter(datas=training,is_hard_augm=True,hard_augm='BLK',is_multiple_augm=True,soft_order=['RN','CR','L2R'],single_augm='')
 #PASS
