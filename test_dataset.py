9aec3fbe3dbaf819e303e8a89105062e549c9e32
Nicola Braile <nicola.braile@icloud.com>
2022-02-13 16:37:16 +0100

    Testing augmenter on Dataset

--- test_dataset.py
+++ test_dataset.py
@@ -1,14 +1,37 @@
 import numpy as np
 from augm.augment import *
+from augm.augmenter import *
 
 data = np.load('dataset/nyc_taxi.npz')
 
-training = data['training'][:200]
+training = data['training'][:50]
 training = np.expand_dims(training, axis=1)
 print(training.shape)
 
+
+#TESTING CODE for Augment.py
+
+'''
 left2rightFlip(training, True)
 random_noise(training)
 crop_resize(training)
 blockout(training,50)
 magnitude_warping(training,2,3)
+'''
+
+#TESTING CODE for Augmenter.py
+
+#Notes:
+#CR must be checked because of zig zag visualitazion ( it may not be a fault )
+
+#augmenter(datas=training,is_hard_augm=False,hard_augm='',is_multiple_augm=True,soft_order=['CR','RN','L2R'],single_augm='') 
+#PASS
+
+#augmenter(datas=training,is_hard_augm=False,hard_augm='',is_multiple_augm=False,soft_order=[],single_augm='RN')
+#PASS
+
+#augmenter(datas=training,is_hard_augm=True,hard_augm='MW',is_multiple_augm=False,soft_order=[],single_augm='L2R')
+#PASS 
+
+#augmenter(datas=training,is_hard_augm=True,hard_augm='BLK',is_multiple_augm=True,soft_order=['RN','CR','L2R'],single_augm='')
+#PASS
